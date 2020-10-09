1f12f3623c0a84eb5a2486a9800c3776b68dd333
Zhiwei Wang <wzw1998@mail.ustc.edu.cn>
2020-10-09 08:25:38 +0800

    R program

--- R/ReadData.R
+++ R/ReadData.R
@@ -8,4 +8,4 @@ gdata[1:6, gname[1:10], with = F]
 # 只取基因型
 gdata <- gdata[ , !c("FID", "IID", "PAT", "MAT", "SEX", "PHENOTYPE")]
 gname <- colnames(gdata)
-gdata[c(1:6), gname[1:6], with = F]
+gdata[1:6, gname[1:6], with = F]
