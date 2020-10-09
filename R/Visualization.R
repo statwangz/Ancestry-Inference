2bee83022f68475ffca053de4f25097ca9dc58fb
Zhiwei Wang <wzw1998@mail.ustc.edu.cn>
2020-10-09 14:38:57 +0800

    update ggplot

--- R/Visualization.R
+++ R/Visualization.R
@@ -16,6 +16,8 @@ pop_k4 <- pam(E_theta, k = 4)
 table(pop_k4$clustering)
 
 # 绘图
-ggplot(data = NULL, aes(x = PC_1, y = PC_2)) +
-  geom_point(color = pop_k4$cluster + 1) +
-  labs(title="PCA", x = "PC 1", y = "PC 2")
+p <- ggplot(data = NULL, aes(x = PC_1, y = PC_2))
+p + geom_point(color = pop_k4$clustering + 1, alpha = 1/2, size = 2) +
+  scale_x_continuous(name = "PC 1", limits = range(PC_1)) +
+  scale_y_continuous(name = "PC 2", limits = range(PC_2)) +
+  labs(title="PCA")
