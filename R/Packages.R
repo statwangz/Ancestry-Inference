f518c1169e4c6796123f4eba720d502e6cde8d52
Zhiwei Wang <wzw1998@mail.ustc.edu.cn>
2020-10-13 10:02:19 +0800

    genio

--- R/Packages.R
+++ R/Packages.R
@@ -1,5 +1,11 @@
 # 处理大量数据时可以用"data.table"包里的"fread"函数
+install("data.table")
 library(data.table)
 
+# 聚类分析
+install.packages("cluster")
 library(cluster)
+
+# 绘图
+install.packages("ggplot2")
 library(ggplot2)
