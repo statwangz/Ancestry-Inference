b0e946e6acdf25e5a6acb35303376e908b2e3bd7
Zhiwei Wang <wzw1998@mail.ustc.edu.cn>
2020-10-24 14:16:42 +0800

    Lower Bound

--- R/Functions.R
+++ R/Functions.R
@@ -1,7 +1,7 @@
 # 用于判断是否收敛
 DMatrix <- function(matrix_new, matrix_old){
-  d <- as.vector(matrix_new - matrix_old)
-  sum(abs(d))/sum(abs(matrix_new))
+  d <- abs(matrix_new - matrix_old)
+  sum(d)/sum(abs(matrix_new))
 }
 
 # X ~ Beta，计算 ln(X) 的期望
@@ -21,4 +21,35 @@ ELogDir <- function(dir){
 LogSumExp <- function(x){
   x <- x - max(x)
   exp(x - log(sum(exp(x))))
+}
+
+# 计算 Lower Bound
+LowerBound <- function(g_data, theta, beta, K, N, L){
+  
+  E_1 <- sum(as.vector(a - beta[ , , 1]) * as.vector(apply(beta, MARGIN = c(1, 2), ELogBeta1)))
+  
+  E_2 <- sum(as.vector(b - beta[ , , 2]) * as.vector(apply(beta, MARGIN = c(1, 2), ELogBeta2)))
+  
+  E_3 <- sum(as.vector(t(c - theta)) * as.vector(apply(theta, MARGIN = 1, FUN = ELogDir)))
+  
+  e_log_dir <- apply(theta, MARGIN = 1, FUN = ELogDir) # 输出结果每一列为每个人的 E_Log_Dir，K*N
+  e_log_beta_1 <-  apply(beta, MARGIN = c(1, 2), FUN = ELogBeta1) # K*L
+  e_log_beta_2 <-  apply(beta, MARGIN = c(1, 2), FUN = ELogBeta2)
+  
+  x <- array(as.vector(apply(e_log_beta_1, MARGIN = 2, FUN = function(x){x + e_log_dir})), dim = c(K, N, L)) # 输出结果每一列为每个基因的信息，相当于对每个基因抽样分析后得到的x矩阵
+  y <- array(as.vector(apply(e_log_beta_2, MARGIN = 2, FUN = function(x){x + e_log_dir})), dim = c(K, N, L))
+  
+  phi <- apply(x, MARGIN = c(2,3), FUN = LogSumExp) # K*N*L
+  xi <- apply(y, MARGIN = c(2,3), FUN = LogSumExp)
+  
+  e_log_dir_comp <- array(rep(e_log_dir, L), dim = c(K, N, L))
+  e_log_beta_1_comp <- array(t(apply(array(rep(e_log_beta_1, N), dim = c(K, L, N)), MARGIN = 1, t)), dim = c(K, N, L))
+  e_log_beta_2_comp <- array(t(apply(array(rep(e_log_beta_2, N), dim = c(K, L, N)), MARGIN = 1, t)), dim = c(K, N, L))
+  
+  
+  E_4 <- sum(as.vector(g_data) * as.vector(apply(array(as.vector(phi) * as.vector(e_log_dir_comp + e_log_beta_1_comp - log(phi)), dim = c(K, N, L)), MARGIN = c(2, 3), sum)))
+  E_5 <- sum(as.vector(2 - g_data) * as.vector(apply(array(as.vector(xi) * as.vector(e_log_dir_comp + e_log_beta_2_comp - log(xi)), dim = c(K, N, L)), MARGIN = c(2, 3), sum)))
+  
+  E_1 + E_2 + E_3 + E_4 + E_5
+  
 }
